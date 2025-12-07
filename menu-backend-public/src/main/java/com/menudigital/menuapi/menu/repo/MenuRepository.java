4c3c19ba0c625ed7b68069787c78215902ff467a
Alex Borja <134891286+bm16036@users.noreply.github.com>
2025-12-07 03:43:58 -0600

    Filter company menus to active entries

--- menu-backend-public/src/main/java/com/menudigital/menuapi/menu/repo/MenuRepository.java
+++ menu-backend-public/src/main/java/com/menudigital/menuapi/menu/repo/MenuRepository.java
@@ -1,1 +1,14 @@
-package com.menudigital.menuapi.menu.repo; import com.menudigital.menuapi.menu.domain.Menu; import org.springframework.data.jpa.repository.JpaRepository; import java.util.*; public interface MenuRepository extends JpaRepository<Menu, UUID>{ List<Menu> findByCompanyIdOrderByNameAsc(UUID companyId);}
+package com.menudigital.menuapi.menu.repo;
+
+import com.menudigital.menuapi.menu.domain.Menu;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MenuRepository extends JpaRepository<Menu, UUID> {
+
+    List<Menu> findByCompanyIdOrderByNameAsc(UUID companyId);
+
+    List<Menu> findByCompanyIdAndActiveTrueOrderByNameAsc(UUID companyId);
+}
